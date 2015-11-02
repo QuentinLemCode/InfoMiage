a0eba41772a93b75ac8081c17649322a73f9b717
Kévin COT <kekou_91@hotmail.fr>
2015-11-02 10:21:18 +0100

    Mise en place Accueil Front end

--- src/InfoMiage/AppBundle/Controller/AppController.php
+++ src/InfoMiage/AppBundle/Controller/AppController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use InfoMiage\AppBundle\InfoMiageAppBundle;
 
 class AppController extends Controller
 {
@@ -15,6 +16,14 @@ class AppController extends Controller
     public function indexAction(Request $request)
     {
     	//Ici remplacer par import index.html.twig et mettre en place ce dernier
-        return new Response("Hello world !");
+        //return new Response("Hello world !");
+
+    	return $this->render('InfoMiageAppBundle:Accueil:index.html.twig');
+    	
+    }
+    
+    public function articleAction($id, Request $request)
+    {
+    	
     }
 }
