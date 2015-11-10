4e3ad7c1b140aaea2aba7691ece8b63d9e6ba822
Quentin Lem <quentinlemcode@gmail.com>
2015-11-10 09:33:20 +0100

    Mise en place de la solution Sonata.

--- app/autoload.php
+++ app/autoload.php
@@ -7,6 +7,7 @@ use Composer\Autoload\ClassLoader;
  * @var ClassLoader $loader
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
+$loader->add("Application", __DIR__.'/../src/Application');
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
